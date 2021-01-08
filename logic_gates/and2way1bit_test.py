698990ca9da375af3815307a7bec963d5bbe30a4
aalvleo <aalvleo@arte-consultores.com>
2021-01-08 20:48:09 +0000

    Refactored And2Way1Bit Test

--- logic_gates/and2way1bit_test.py
+++ logic_gates/and2way1bit_test.py
@@ -8,6 +8,6 @@ and2Way1Bit = And2Way1Bit()
 for i in range(len(test_input)):
     result = and2Way1Bit.compute(test_input[i][0], test_input[i][1])
     if result != test_output[i]:
-        sys.exit('Error, the input {} should output: {} but got: {}'.format(test_input[i], test_output[i], result))
+        sys.exit('Error, a[{}], b[{}] should output [{}] but got: [{}]'.format(test_input[i][0], test_input[i][1], test_output[i], result))
 
 print("Test for And2Way1bit passed successfully.")
---
+++ logic_gates/mux2way1bit.py
@@ -0,0 +1,10 @@
+
+
+class Mux2Way1Bit:
+
+    def compute(self, sel, a, b):
+        
+        if sel == 0:
+            return a
+        if sel == 1:
+            return b
