53fff44926d8f742472e1e9c11ef829640a88708
aalvleo <aalvleo@arte-consultores.com>
2021-01-08 20:00:58 +0000

    Refactor and2x1

--- and2way1bit_test.py
+++ and2way1bit_test.py
@@ -2,10 +2,10 @@ import sys
 from and2way1bit import And2Way1Bit
 
 test_input = [[0,0,0], [0,1,0], [1,0,0], [1,1,1]]
-and2Way1bit = And2Way1Bit()
+and2Way1Bit = And2Way1Bit()
 
 for t_input in test_input:
-    result = and2Way1bit.compute(t_input[0], t_input[1])
+    result = and2Way1Bit.compute(t_input[0], t_input[1])
     if result != t_input[2]:
         sys.exit('Error, the inputs {} - {}  should output: {}'.format(t_input[0], t_input[1], t_input[2]))
 print("Test for And2Way1bit passed successfully.")
