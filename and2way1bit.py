065b064e1c9021723dcf8bc3842d7d5525dc75f3
aalvleo <aalvleo@arte-consultores.com>
2021-01-08 20:11:23 +0000

    Refactor and2x1

--- and2way1bit.py
+++ and2way1bit.py
@@ -7,7 +7,10 @@ class And2Way1Bit:
         a = _in[0]
         b = _in[1]
 
-        if a == 1 and b == 1:
+        assert len(a) == 1
+        assert len(b) == 1
+
+        if a[0] == 1 and b[0] == 1:
             return 1
         else:
             return 0
--- and2way1bit_test.py
+++ and2way1bit_test.py
@@ -1,13 +1,13 @@
 import sys
 from and2way1bit import And2Way1Bit
 
-test_input = [[0,0], [0,1], [1,0], [1,1]]
+test_input = [[[0],[0]], [[0],[1]], [[1],[0]], [[1],[1]]]
 test_output = [0, 0, 0, 1]
 and2Way1Bit = And2Way1Bit()
 
 for i in range(len(test_input)):
     result = and2Way1Bit.compute(test_input[i])
     if result != test_output[i]:
-        sys.exit('Error, the input {} should output: {} but got: {}'.format(t_input[i], t_output[i], result))
+        sys.exit('Error, the input {} should output: {} but got: {}'.format(test_input[i], test_output[i], result))
 
 print("Test for And2Way1bit passed successfully.")
