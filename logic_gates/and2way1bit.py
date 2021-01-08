1f0a42689c6c6c44d087d46002756efc948bd659
aalvleo <aalvleo@arte-consultores.com>
2021-01-08 20:35:40 +0000

    Refactor And2Way1Bit input

--- logic_gates/and2way1bit.py
+++ logic_gates/and2way1bit.py
@@ -1,16 +1,8 @@
 
 class And2Way1Bit:
-    def compute(self, _in):
 
-        assert len(_in) == 2
-
-        a = _in[0]
-        b = _in[1]
-
-        assert len(a) == 1
-        assert len(b) == 1
-
-        if a[0] == 1 and b[0] == 1:
+    def compute(self, a, b):
+        if a == 1 and b == 1:
             return 1
         else:
             return 0
--- logic_gates/and2way1bit_test.py
+++ logic_gates/and2way1bit_test.py
@@ -1,12 +1,12 @@
 import sys
 from and2way1bit import And2Way1Bit
 
-test_input = [[[0],[0]], [[0],[1]], [[1],[0]], [[1],[1]]]
+test_input = [[0,0], [0,1], [1,0], [1,1]]
 test_output = [0, 0, 0, 1]
 and2Way1Bit = And2Way1Bit()
 
 for i in range(len(test_input)):
-    result = and2Way1Bit.compute(test_input[i])
+    result = and2Way1Bit.compute(test_input[i][0], test_input[i][1])
     if result != test_output[i]:
         sys.exit('Error, the input {} should output: {} but got: {}'.format(test_input[i], test_output[i], result))
 
