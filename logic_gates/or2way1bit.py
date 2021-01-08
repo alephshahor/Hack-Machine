385ad1b36bfc9b9d67ed65a849ac9ea8f441aa1f
aalvleo <aalvleo@arte-consultores.com>
2021-01-08 20:37:57 +0000

    Refactor Or2Way1Bit input

--- logic_gates/or2way1bit.py
+++ logic_gates/or2way1bit.py
@@ -1,16 +1,8 @@
 class Or2Way1Bit:
 
-    def compute(self, _in):
+    def compute(self, a, b):
 
-        assert len(_in) == 2
-
-        a = _in[0]
-        b = _in[1]
-
-        assert len(a) == 1
-        assert len(b) == 1
-
-        if a[0] != 0 or b[0] != 0:
+        if a != 0 or b != 0:
             return 1
         else:
             return 0
--- logic_gates/or2way1bit_test.py
+++ logic_gates/or2way1bit_test.py
@@ -1,12 +1,12 @@
 import sys
 from or2way1bit import Or2Way1Bit
 
-test_input = [[[0],[0]], [[0],[1]], [[1],[0]], [[1],[1]]]
+test_input = [[0,0], [0,1], [1,0], [1,1]]
 test_output = [0,1,1,1]
 or2way1bit = Or2Way1Bit()
 
 for i in range(len(test_input)):
-    result = or2way1bit.compute(test_input[i])
+    result = or2way1bit.compute(test_input[i][0], test_input[i][1])
     if result != test_output[i]:
         sys.exit('Error, the input {} should output: {} but got: {}'.format(test_input[i], test_output[i], result))
 print("Test for Or2Way1Bit passed successfully.")
