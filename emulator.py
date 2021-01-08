edb56ad3eaf8db88811cb93268a167887ecc599d
aalvleo <aalvleo@arte-consultores.com>
2021-01-08 22:27:42 +0000

    Added assertions to Mux2Way16Bit

--- emulator.py
+++
@@ -1,4 +0,0 @@
-from nand2x1 import Nand2x1
-
-n = Nand2x1()
-print(n.compute(0,1))
--- logic_gates/mux2way16bit.py
+++ logic_gates/mux2way16bit.py
@@ -3,5 +3,9 @@ from mux2way1bit import Mux2Way1Bit
 class Mux2Way16Bit:
 
     def compute(self, sel, a, b):
+
+        assert len(a) == 16
+        assert len(b) == 16
+
         mux2Way1Bit = Mux2Way1Bit()
         return mux2Way1Bit.compute(sel, a, b)
