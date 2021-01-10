f1b80f3c7d5fabb0c060cdf6903fd75d074cf706
alephshahor <adrianalvarezleon@gmail.com>
2021-01-10 17:54:29 +0000

    RAM64Bit implemented

--- memory/ram8bit_test.py
+++ memory/ram8bit_test.py
@@ -13,6 +13,6 @@ for i in range(len(test_input)):
     result = ram8Bit.compute(test_input[i], test_address[i], test_load[i])
     if result != test_output[i]:
         sys.exit('Error, in[{}], address[{}], load[{}] should output: [{}] but got: [{}]'
-           .format(test_input[i-1], test_load[i-1], test_address[i-1], test_output[i], result))
+           .format(test_input[i-1], test_address[i-1], test_load[i-1],  test_output[i], result))
 
 print("Test for RAM8Bit passed successfully.")
