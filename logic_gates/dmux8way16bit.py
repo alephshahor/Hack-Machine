db7c2d4e06178eebbb690f8cde14ecea201fad1c
aalvleo <aalvleo@arte-consultores.com>
2021-01-10 14:11:26 +0000

    Refactored Dmux8Way into DMux8Way16Bit

--- logic_gates/dmux8way16bit.py
+++ logic_gates/dmux8way16bit.py
@@ -1,16 +1,16 @@
-from dmux2way import DMux2Way
-from dmux4way import DMux4Way
+from dmux2way16bit import DMux2Way16Bit
+from dmux4way16bit import DMux4Way16Bit
 
 
-class DMux8Way:
+class DMux8Way16Bit:
 
     def compute(self, sel, _in):
 
         assert len(sel) == 3
 
-        dMux2Way = DMux2Way()
-        dMux4Way_1 = DMux4Way()
-        dMux4Way_2 = DMux4Way()
+        dMux2Way = DMux2Way16Bit()
+        dMux4Way_1 = DMux4Way16Bit()
+        dMux4Way_2 = DMux4Way16Bit()
 
         dMux2Way_out = dMux2Way.compute(sel[0], _in)
         dMux4Way_1_out = dMux4Way_1.compute(sel[1:], dMux2Way_out[0])
