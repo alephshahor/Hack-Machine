e214507c9b62e738d0c55a5607be748c19f00182
alephshahor <adrianalvarezleon@gmail.com>
2021-02-21 17:51:15 +0000

    feat(all): Implement conversions "dec_to_bin_arr" and "bin_to_dec"

--- hack_machine/utils/conversions.py
+++ hack_machine/utils/conversions.py
@@ -1,5 +1,11 @@
-
-
 def dec_to_bin(n_bit, dec_number):
     bin_str_format = "{0:0"+str(n_bit)+"b}"
     return bin_str_format.format(dec_number)
+
+def dec_to_bin_arr(n_bit, dec_number):
+    bin_str = dec_to_bin(n_bit, dec_number)
+    bin_str_arr = list(bin_str)
+    return [int(b_num) for b_num in bin_str_arr]
+
+def bin_to_dec(bin_number):
+    return int(bin_number, 2)
