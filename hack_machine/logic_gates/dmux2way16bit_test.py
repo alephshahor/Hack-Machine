02905170d3a93d0725a728552729192fee5aa114
alephshahor <adrianalvarezleon@gmail.com>
2021-02-21 18:19:01 +0000

    refactor(logic_gates): Refactor dmux2way16bit_test

--- hack_machine/logic_gates/dmux2way16bit_test.py
+++ hack_machine/logic_gates/dmux2way16bit_test.py
@@ -1,15 +1,32 @@
 import sys
-from dmux2way16bit import DMux2Way16Bit
+from hack_machine.logic_gates.dmux2way16bit import DMux2Way16Bit
+from hack_machine.utils import conversions
+from random import randrange
 
-test_input = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]]
-test_sel = [0, 1]
-test_output = [[[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]], [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]]]
+n_iterations = 100
+n_bit = 16
+random_ceil = conversions.bin_to_dec('1' * 16)
 
 dMux2Way16Bit = DMux2Way16Bit()
+zero = conversions.dec_to_bin_arr(n_bit, 0)
 
-for i in range(len(test_input)):
-    result = dMux2Way16Bit.compute(test_sel[i], test_input[i])
-    if result != test_output[i]:
-        sys.exit('Error, sel[{}], in{} should output: {} but got: {}'
-           .format(test_sel[i], test_input[i], test_output[i], result))
-print("Test for Dmux2Way16bit passed successfully.")
+
+def test_dmux2way16bit():
+    for i in range(n_iterations):
+        selector_input = i % 2
+
+        input_dec = randrange(random_ceil)
+        input_bin = conversions.dec_to_bin_arr(n_bit, input_dec)
+
+        result = dMux2Way16Bit.compute(selector_input, input_bin)
+
+        if selector_input:
+            assert result == [zero, input_bin], 'Error, sel[{}], in{} should output: {} but got: {}'.format(selector_input,
+                                                                                                            input_dec,
+                                                                                                            [0, input_dec],
+                                                                                                            [conversions.bin_arr_to_dec(result[0]), conversions.bin_arr_to_dec(result[1])])
+        else:
+            assert result == [input_bin, zero], 'Error, sel[{}], in{} should output: {} but got: {}'.format(selector_input,
+                                                                                                            input_bin,
+                                                                                                            [zero, input_bin],
+                                                                                                            [conversions.bin_arr_to_dec(result[0]),conversions.bin_arr_to_dec(result[1])])
--- hack_machine/utils/conversions.py
+++ hack_machine/utils/conversions.py
@@ -1,11 +1,18 @@
 def dec_to_bin(n_bit, dec_number):
-    bin_str_format = "{0:0"+str(n_bit)+"b}"
+    bin_str_format = "{0:0" + str(n_bit) + "b}"
     return bin_str_format.format(dec_number)
+
 
 def dec_to_bin_arr(n_bit, dec_number):
     bin_str = dec_to_bin(n_bit, dec_number)
     bin_str_arr = list(bin_str)
     return [int(b_num) for b_num in bin_str_arr]
 
+
 def bin_to_dec(bin_number):
     return int(bin_number, 2)
+
+
+def bin_arr_to_dec(bin_arr):
+    bin_arr_str = ''.join([str(x) for x in bin_arr])
+    return int(bin_arr_str, 2)
