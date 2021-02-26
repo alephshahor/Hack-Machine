8b96cbcbd598391f47b08d23fa7973569275410c
alephshahor <adrianalvarezleon@gmail.com>
2021-02-26 19:09:41 +0000

    refactor(logic_gates): Refactor mux2way1bit_test

--- hack_machine/logic_gates/mux2way1bit_test.py
+++ hack_machine/logic_gates/mux2way1bit_test.py
@@ -1,14 +1,17 @@
-import sys
-from mux2way1bit import Mux2Way1Bit
+import pytest
+from hack_machine.logic_gates.mux2way1bit import Mux2Way1Bit
 
 test_input = [[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0], [1,0,1], [1,1,0], [1,1,1]]
 test_output = [0,0,1,1,0,1,0,1]
 
 mux2Way1Bit = Mux2Way1Bit()
 
-for i in range(len(test_input)):
-    result = mux2Way1Bit.compute(test_input[i][0], test_input[i][1], test_input[i][2])
-    if result != test_output[i]:
-        sys.exit('Error, sel[{}], a[{}], b[{}] should output: [{}] but got: [{}]'
-           .format(test_input[i][0], test_input[i][1], test_input[i][2], test_output[i], result))
-print("Test for Mux2Way1Bit passed successfully.")
+
+def test_mux2way1bit():
+    for i in range(len(test_input)):
+        result = mux2Way1Bit.compute(test_input[i][0], test_input[i][1], test_input[i][2])
+        assert result == test_output[i], 'Error, sel[{}], a[{}], b[{}] should output: [{}] but got: [{}]'.format(test_input[i][0],
+                                                                                                                 test_input[i][1],
+                                                                                                                 test_input[i][2],
+                                                                                                                 test_output[i],
+                                                                                                                 result)
