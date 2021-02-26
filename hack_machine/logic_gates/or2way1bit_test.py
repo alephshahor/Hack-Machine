4ef6c5ad1fd98c2126b3f659287fba8d5e477d60
alephshahor <adrianalvarezleon@gmail.com>
2021-02-26 22:35:13 +0000

    refactor(logic_gates): Implement or2way1bit_test

--- hack_machine/logic_gates/or2way1bit_test.py
+++ hack_machine/logic_gates/or2way1bit_test.py
@@ -1,13 +1,16 @@
 import sys
-from or2way1bit import Or2Way1Bit
+from hack_machine.logic_gates.or2way1bit import Or2Way1Bit
 
-test_input = [[0,0], [0,1], [1,0], [1,1]]
-test_output = [0,1,1,1]
+test_a = [0, 0, 1, 1]
+test_b = [0, 1, 0, 1]
+test_output = [0, 1, 1, 1]
 
 or2way1bit = Or2Way1Bit()
 
-for i in range(len(test_input)):
-    result = or2way1bit.compute(test_input[i][0], test_input[i][1])
-    if result != test_output[i]:
-        sys.exit('Error, a[{}], b[{}] should output: [{}] but got: [{}]'.format(test_input[i][0], test_input[i][1], test_output[i], result))
-print("Test for Or2Way1Bit passed successfully.")
+def test_or2way1bit():
+    for i in range(len(test_a)):
+        result = or2way1bit.compute(test_a[i], test_b[i])
+        assert result == test_output[i], 'Error, a: {},  b: {} should output: {} but got: {}'.format(test_a[i],
+                                                                                                     test_b[i],
+                                                                                                     test_output[i],
+                                                                                                     result)
