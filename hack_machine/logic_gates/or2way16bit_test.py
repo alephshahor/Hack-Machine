e5f3a765b8926db1d60fc90a8466336f4a772c11
alephshahor <adrianalvarezleon@gmail.com>
2021-02-26 22:52:52 +0000

    refactor(logic_gates): Refactor or2way16bit_test

--- hack_machine/logic_gates/or2way16bit_test.py
+++ hack_machine/logic_gates/or2way16bit_test.py
@@ -18,7 +18,7 @@ or2Way16Bit = Or2Way16Bit()
 def test_or2way16bit():
     for i in range(len(a)):
         result = or2Way16Bit.compute(a[i], b[i])
-        assert result == output[i], 'Error, a[{}], b[{}] should output: [{}] but got: [{}]'.format(a[i],
+        assert result == output[i], 'Error, a: {}, b: {} should output: {} but got: {}'.format(a[i],
                                                                                                    b[i],
                                                                                                    output[i],
                                                                                                    result)
