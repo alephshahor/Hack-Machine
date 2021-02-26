d4179a980fe314c09a97f033669c911f52494e87
alephshahor <adrianalvarezleon@gmail.com>
2021-02-26 19:27:21 +0000

    refactor(logic_gates): Refactor mux2way1bit_test

--- hack_machine/logic_gates/mux2way1bit_test.py
+++ hack_machine/logic_gates/mux2way1bit_test.py
@@ -2,17 +2,22 @@ import pytest
 from hack_machine.logic_gates.mux2way1bit import Mux2Way1Bit
 
 test_input = [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1], [1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]]
-test_output = [0, 0, 1, 1, 0, 1, 0, 1]
 
+input_a = [0, 0, 1, 1, 0, 0, 1, 1]
+input_b = [0, 1, 0, 1, 0, 1, 0, 1]
+selector = [0, 0, 0, 0, 1, 1, 1, 1]
+output = [0, 0, 1, 1, 0, 1, 0, 1]
+
+n_iterations = len(input_a)
 mux2Way1Bit = Mux2Way1Bit()
 
 
 def test_mux2way1bit():
-    for i in range(len(test_input)):
-        result = mux2Way1Bit.compute(test_input[i][0], test_input[i][1], test_input[i][2])
-        assert result == test_output[i], 'Error, sel[{}], a[{}], b[{}] should output: [{}] but got: [{}]'.format(
-            test_input[i][0],
-            test_input[i][1],
-            test_input[i][2],
-            test_output[i],
+    for i in range(n_iterations):
+        result = mux2Way1Bit.compute(selector[i], input_a[i], input_b[i])
+        assert result == output[i], 'Error, sel[{}], a[{}], b[{}] should output: [{}] but got: [{}]'.format(
+            selector[i],
+            input_a[i],
+            input_b[i],
+            output[i],
             result)
