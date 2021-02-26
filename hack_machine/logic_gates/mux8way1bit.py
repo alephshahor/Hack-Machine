f52356d735e40adf60c07cd66da2e938ef1b9ec8
alephshahor <adrianalvarezleon@gmail.com>
2021-02-26 21:46:55 +0000

    feat(logic_gates): Implement mux8way1bit

---
+++ hack_machine/logic_gates/mux8way1bit.py
@@ -0,0 +1,20 @@
+from hack_machine.logic_gates.mux2way1bit import Mux2Way1Bit
+from hack_machine.logic_gates.mux4way1bit import Mux4Way1Bit
+
+class Mux8Way1Bit:
+
+    def compute(self, sel, a, b, c, d, e, f, g, h):
+
+        assert len(sel) == 3
+
+        # 0 1 1
+        mux4way1bit_1 = Mux4Way1Bit()
+        mux4way1bit_1_out = mux4way1bit_1.compute(sel[1:], a, b, c, d)
+
+        mux4way1bit_2 = Mux4Way1Bit()
+        mux4way1bit_2_out = mux4way1bit_2.compute(sel[1:], e, f, g, h)
+
+        mux2way1bit = Mux2Way1Bit()
+        mux2way1bit_out = mux2way1bit.compute(sel[0], mux4way1bit_1_out, mux4way1bit_2_out)
+
+        return mux2way1bit_out
