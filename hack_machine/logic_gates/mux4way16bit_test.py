cbba91230da656c71970ab0b1d0bba0b122b6d3f
alephshahor <adrianalvarezleon@gmail.com>
2021-02-26 20:31:17 +0000

    refactor(logic_gates): Refactor mux4way16bit_test

--- hack_machine/logic_gates/mux4way16bit_test.py
+++ hack_machine/logic_gates/mux4way16bit_test.py
@@ -1,33 +1,71 @@
 import pytest
 from hack_machine.logic_gates.mux4way16bit import Mux4Way16Bit
+from hack_machine.utils import conversions
+from random import randrange
 
-test_a_input = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]]
-
-test_b_input = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]]
-
-test_c_input = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0]]
-
-test_d_input = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]]
-
-test_sel_input = [[0, 0], [0, 1], [1, 0], [1, 1]]
-
-test_output = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
-               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]]
+n_iterations = 400
+n_bit = 16
+random_ceil = conversions.bin_to_dec('1' * 16)
 
 mux4Way16Bit = Mux4Way16Bit()
 
 
 def test_mux4way16bit():
-    for i in range(len(test_a_input)):
-        result = mux4Way16Bit.compute(test_sel_input[i], test_a_input[i], test_b_input[i], test_c_input[i], test_d_input[i])
-        assert result == test_output[i], 'Error, the input sel{}, a{}, b{}, c{}, d{} should output: {} but instead got: {}'.format(test_sel_input[i],
-                                                                                                                                   test_a_input[i],
-                                                                                                                                   test_b_input[i],
-                                                                                                                                   test_c_input[i],
-                                                                                                                                   test_d_input[i],
-                                                                                                                                   test_output[i],
+    for i in range(n_iterations):
+        sel = [randrange(2), randrange(2)]
+
+        a_dec = randrange(random_ceil)
+        a_bin = conversions.dec_to_bin_arr(n_bit, a_dec)
+
+        b_dec = randrange(random_ceil)
+        b_bin = conversions.dec_to_bin_arr(n_bit, b_dec)
+
+        c_dec = randrange(random_ceil)
+        c_bin = conversions.dec_to_bin_arr(n_bit, c_dec)
+
+        d_dec = randrange(random_ceil)
+        d_bin = conversions.dec_to_bin_arr(n_bit, d_dec)
+
+        result = mux4Way16Bit.compute(sel, a_bin, b_bin, c_bin, d_bin)
+
+        if sel == [0, 0]:
+            expected_out = a_bin
+            assert expected_out == result, 'Error, the input sel: {}, a: {}, b: {}, c: {}, d: {} should output: {} but instead got: {}'.format(sel,
+                                                                                                                                   a_dec,
+                                                                                                                                   b_dec,
+                                                                                                                                   c_dec,
+                                                                                                                                   d_dec,
+                                                                                                                                   expected_out,
                                                                                                                                    result)
+        elif sel == [0, 1]:
+            expected_out = b_bin
+            assert expected_out == result, 'Error, the input sel: {}, a: {}, b: {}, c: {}, d: {} should output: {} but instead got: {}'.format(
+                sel,
+                a_dec,
+                b_dec,
+                c_dec,
+                d_dec,
+                expected_out,
+                result)
+
+        elif sel == [1, 0]:
+            expected_out = c_bin
+            assert expected_out == result, 'Error, the input sel: {}, a: {}, b: {}, c: {}, d: {} should output: {} but instead got: {}'.format(
+                sel,
+                a_dec,
+                b_dec,
+                c_dec,
+                d_dec,
+                expected_out,
+                result)
+
+        elif sel == [1, 1]:
+            expected_out = d_bin
+            assert expected_out == result, 'Error, the input sel: {}, a: {}, b: {}, c: {}, d: {} should output: {} but instead got: {}'.format(
+                sel,
+                a_dec,
+                b_dec,
+                c_dec,
+                d_dec,
+                expected_out,
+                result)
