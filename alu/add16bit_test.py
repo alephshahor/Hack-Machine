f676c038ddaa19107b9a21604a75700c90e57de5
alephshahor <adrianalvarezleon@gmail.com>
2021-02-27 19:22:28 +0000

    refactor(alu): Refactor add16bit_test

--- alu/add16bit_test.py
+++ alu/add16bit_test.py
@@ -1,22 +1,21 @@
+import pytest
 import sys
 from add16bit import Add16Bit
 
-
-test_a_input = [[0,1,0,0,1,0,0,0,0,0,0,1,0,0,1,0], [0,1,0,0,0,0,0,1,1,1,0,1,0,0,1,0]]
-test_b_input = [[0,1,1,0,0,1,0,0,0,0,1,1,0,0,1,1], [0,1,0,1,0,0,0,1,1,1,0,0,1,0,1,0]]
-test_output  = [[1,0,1,0,1,1,0,0,0,1,0,0,0,1,0,1], [1,0,0,1,0,0,1,1,1,0,0,1,1,1,0,0]]
+test_a_input = [[0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0], [0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0]]
+test_b_input = [[0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1], [0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0]]
+test_output = [[1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1], [1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0]]
 
 add16Bit = Add16Bit()
 
-for i in range(len(test_a_input)):
 
-    a = test_a_input[i]
-    b = test_b_input[i]
+def test_add16bit():
 
-    result = add16Bit.compute(a,b)
+    for i in range(len(test_a_input)):
 
-    if result != test_output[i]:
-        sys.exit('Error, a{}, b{} should output {} but got: {}.'
-           .format(a,b,test_output[i],result))
+        a = test_a_input[i]
+        b = test_b_input[i]
 
-print("Test for Add16Bit passed successfully.")
+        result = add16Bit.compute(a, b)
+
+        assert result == test_output[i], 'Error, a: {}, b: {} should output {} but got: {}.'.format(a, b, test_output[i], result)
