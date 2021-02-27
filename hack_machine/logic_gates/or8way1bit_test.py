e87a857321e0d1d1a486edb93cadd561181a6f1a
alephshahor <adrianalvarezleon@gmail.com>
2021-02-27 10:40:22 +0000

    refactor(logic_gates): Refactor or8way1bit_test

--- hack_machine/logic_gates/or8way1bit_test.py
+++ hack_machine/logic_gates/or8way1bit_test.py
@@ -1,17 +1,18 @@
-import sys
-from or8way1bit import Or8Way1Bit
+import pytest
+from hack_machine.logic_gates.or8way1bit import Or8Way1Bit
 
-test_input = [[0,1,0,0,0,0,1,0], [0,0,0,0,0,0,0,0], [0,1,1,0,0,0,1,1]]
+test_input = [[0, 1, 0, 0, 0, 0, 1, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 1, 1, 0, 0, 0, 1, 1]]
 
-test_output = [1,0,1]
+test_output = [1, 0, 1]
 
 or8Way1Bit = Or8Way1Bit()
 
-for i in range(len(test_input)):
-    result = or8Way1Bit.compute(test_input[i])
 
-    if result != test_output[i]:
-        sys.exit('Error, in{} should output: {} but instead got: {}'
-           .format(test_input[i], test_output[i], result))
-
-print("Test for Or8Way1Bit passed successfully.")
+def test_or8way1bit():
+    for i in range(len(test_input)):
+        result = or8Way1Bit.compute(test_input[i])
+        assert result == test_output[i], 'Error, in: {} should output: {} but instead got: {}'.format(test_input[i],
+                                                                                                      test_output[i],
+                                                                                                      result)
