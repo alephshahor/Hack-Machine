ef83eb39b8cbff491aafe75bfe53936ec6a6c92f
alephshahor <adrianalvarezleon@gmail.com>
2021-02-27 19:18:08 +0000

    refactor(alu): Refactor full_adder_test

--- alu/full_adder_test.py
+++ alu/full_adder_test.py
@@ -1,20 +1,25 @@
-import sys
+import pytest
 from full_adder import FullAdder
 
-test_input = [[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1]]
-test_output = [[0,0],[1,0],[1,0],[0,1],[1,0],[0,1],[0,1],[1,1]]
+test_input = [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1], [1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]]
+test_output = [[0, 0], [1, 0], [1, 0], [0, 1], [1, 0], [0, 1], [0, 1], [1, 1]]
 
 full_adder = FullAdder()
 
-for i in range(len(test_input)):
-    a = test_input[i][0]
-    b = test_input[i][1]
-    c = test_input[i][2]
 
-    sum, carry = full_adder.compute(a,b,c)
+def test_full_adder():
+    for i in range(len(test_input)):
+        a = test_input[i][0]
+        b = test_input[i][1]
+        c = test_input[i][2]
 
-    if sum != test_output[i][0] or carry != test_output[i][1]:
-        sys.exit('Error, a[{}], b[{}], c[{}] should output [{}], [{}] but got: [{}]. [{}]'
-           .format(a,b,c,test_output[i][0], test_output[i][1], sum, carry))
+        sum, carry = full_adder.compute(a, b, c)
 
-print("Test for FullAdder passed successfully.")
+        assert sum == test_output[i][0] or carry == test_output[i][1], 'Error, a: {}, b: {}, c: {} should output' \
+                                                                       '{}, {} but got: {}, {}'.format(a,
+                                                                                                       b,
+                                                                                                       c,
+                                                                                                       test_output[i][0],
+                                                                                                       test_output[i][1],
+                                                                                                       sum,
+                                                                                                       carry)
