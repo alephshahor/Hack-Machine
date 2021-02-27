5313db345c7a344c3a4ef8f93f98ed4b64bcb0db
alephshahor <adrianalvarezleon@gmail.com>
2021-02-27 19:19:52 +0000

    refactor(alu): Refactor inc16_test

--- alu/inc16_test.py
+++ alu/inc16_test.py
@@ -1,18 +1,18 @@
+import pytest
 import sys
 from inc16 import Inc16
 
-
-test_input = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1], [0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0], [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
-test_output= [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0], [0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1], [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+test_input = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0], [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+test_output = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1], [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
 inc16 = Inc16()
 
-for i in range(len(test_input)):
 
-    result = inc16.compute(test_input[i])
-
-    if result != test_output[i]:
-        sys.exit('Error, in{} should output {} but got: {}'
-           .format(test_input[i], test_output[i], result))
-
-print("Test for Inc16 passed successfully.")
+def test_inc16():
+    for i in range(len(test_input)):
+        result = inc16.compute(test_input[i])
+        assert result == test_output[i], 'Error, in{} should output {} but got: {}'.format(test_input[i],
+                                                                                           test_output[i],
+                                                                                           result)
