09fb8b59e857b8d1a530faa122bb4a10d05f1144
alephshahor <adrianalvarezleon@gmail.com>
2021-02-27 16:35:46 +0000

    refactor(memory): Refactor Dff

--- memory/dff_test.py
+++ memory/dff_test.py
@@ -1,16 +1,17 @@
 import sys
+import os
 from dff import DFF
 
-test_input = [0,1,0,1]
-test_output = [0,0,1,0]
+sys.path.append(os.environ['ROOT_FOLDER'] + '/logic_gates')
+
+test_input = [0, 1, 0, 1]
+test_output = [0, 0, 1, 0]
 
 dff = DFF()
 
-for i in range(len(test_input)):
-
-    result = dff.compute(test_input[i])
-    if result != test_output[i]:
-        sys.exit('Error, in[{}], should output: [{}] but got: [{}]'
-           .format(test_input[i-1], test_output[i], result))
-
-print("Test for DFF passed successfully.")
+def test_dff():
+    for i in range(len(test_input)):
+        result = dff.compute(test_input[i])
+        assert result == test_output[i], 'Error, in: {}, should output: {} but got: {}'.format(test_input[i - 1],
+                                                                                               test_output[i],
+                                                                                               result)
