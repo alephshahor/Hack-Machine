a64715e3619ad95e3e31a696a71b70f12efd7b8d
alephshahor <adrianalvarezleon@gmail.com>
2021-02-27 19:01:50 +0000

    refactor(memory): Refactor ram32kbit

--- memory/ram32kbit_test.py
+++ memory/ram32kbit_test.py
@@ -1,4 +1,4 @@
-import sys
+import pytest
 from ram32kbit import RAM32KBit
 
 test_input = [i + 1 for i in range(16)] 
@@ -14,22 +14,24 @@ def to_binary_arr(num, n_bits):
     bin_num_arr = [ int(bit) for bit in bin_num_str_arr ]
     return bin_num_arr
 
-for j in range(2):
-    offset = 0
-    if j == 1:
-        offset = 8
-    test_address = [ i + ( 16384 * (j + 1) - offset) for i in range(8) ] * 2
-    for i in range(len(test_input)):
-        _in  = to_binary_arr(test_input[i], 16)
-        addr = to_binary_arr(test_address[i], 15)
-        load = test_load[i]
-        result = ram32kBit.compute(_in, addr, load)
-        exp_result = to_binary_arr(test_result[i], 16)
+def test_ram32kbit():
 
-        if result != exp_result:
-            sys.exit('Error, in[{}], address[{}], load[{}] should output: [{}] but got: [{}]'
-               .format(_in, addr, load, exp_result, result))
-    print("Memory " + str(j) + " passed successfully")
-print("Test for RAM32KBit passed successfully")
+    for j in range(2):
+        offset = 0
+        if j == 1:
+            offset = 8
+        test_address = [ i + ( 16384 * (j + 1) - offset) for i in range(8) ] * 2
+        for i in range(len(test_input)):
+            _in  = to_binary_arr(test_input[i], 16)
+            addr = to_binary_arr(test_address[i], 15)
+            load = test_load[i]
+            result = ram32kBit.compute(_in, addr, load)
+            exp_result = to_binary_arr(test_result[i], 16)
+
+            assert result == exp_result, 'Error, in: {}, address: {}, load: {} should output: {} but got: {}'.format(_in,
+                                                                                                                     addr,
+                                                                                                                     load,
+                                                                                                                     exp_result,
+                                                                                                                     result)
 
 
