a858cfe4fc37c8287ba609ad3aa0090daffa8805
alephshahor <adrianalvarezleon@gmail.com>
2021-03-14 15:27:34 +0000

    refactor(cpu): Fix bugs in cpu

--- cpu/cpu.py
+++ cpu/cpu.py
@@ -3,6 +3,7 @@ import os
 
 sys.path.append(os.environ['ROOT_FOLDER'] + '/logic_gates')
 from mux2way16bit import Mux2Way16Bit
+from mux2way1bit import Mux2Way1Bit
 
 sys.path.append(os.environ['ROOT_FOLDER'] + '/memory')
 from ram32kbit import RAM32KBit
@@ -10,48 +11,55 @@ from register16bit import Register16Bit
 from pc import Pc
 
 sys.path.append(os.environ['ROOT_FOLDER'] + '/alu')
-from cpu import Alu
-from jumpdecoder import JumpDecoder
+from alu import Alu
 
 sys.path.append(os.environ['ROOT_FOLDER'] + '/cpu')
 from decoder import Decoder
+from jump_decoder import JumpDecoder
 
 zero = [0] * 16
 one = zero[:15] + [1]
 
-instruction_memory = RAM32KBit()
-data_memory = RAM32KBit()
-decoder = Decoder()
-alu = Alu()
-a_register = Register16Bit()
-d_register = Register16Bit()
-jump_decoder = JumpDecoder()
-pc = Pc()
+# TODO: Decoder fuera!
+class CPU:
 
-a_out = zero
-d_out = zero
-alu_out = zero
-jump_decoder_out = 0
+    def __init__(self):
+        #instruction_memory = RAM32KBit()
+        #data_memory = RAM32KBit()
+        self.decoder = Decoder()
+        self.alu = Alu()
+        self.a_register = Register16Bit()
+        self.d_register = Register16Bit()
+        self.jump_decoder = JumpDecoder()
+        self.pc = Pc()
+        self.a_out = zero
+        self.d_out = zero
+        self.alu_out = zero
+        self.jump_decoder_out = 0 # Lo puedes quitar de aqui creo
+        self.load_pc = 0
+        self.a_or_c_instr_mux = Mux2Way16Bit()
+        self.load_a_mux = Mux2Way1Bit()
+        self.a_or_m_mux = Mux2Way16Bit()
+        self.load_d_mux = Mux2Way1Bit()
+        self.load_jump_mux = Mux2Way1Bit()
 
-a_or_c_instr_mux = Mux2Way16Bit()
-load_a_mux = Mux2Way16Bit()
-a_or_m_mux = Mux2Way16Bit()
-load_d_mux = Mux2Way16Bit()
+    def compute(self, instr, mem_in, reset):
+        
+        x, a, c1, c2, c3, c4, c5, c6, d1, d2, d3, j1, j2, j3 = self.decoder.compute(instr)
 
-instr_dir = pc.compute(a_out, 1, jump_decoder_out, 0)
-instr = instruction_memory.compute(zero, instr_dir, 0)
-mem_in = data_memory.compute(alu_out, a_out, d3)
-x, a, c1, c2, c3, c4, c5, c6, d1, d2, d3, j1, j2, j3 = decoder.compute(instr)
+        a_in = self.a_or_c_instr_mux.compute(x, instr, self.alu_out)
+        a_load = self.load_a_mux.compute(x, 1, d1)
+        self.a_out = self.a_register.compute(a_in, a_load)
 
-a_in = a_or_c_instr_mux.compute(x, alu_out, instr)
-a_load = load_a_mux.compute(x, one, d1)
-a_out = a_register.compute(a_in, a_load)
+        a_or_m_mux_out = self.a_or_m_mux.compute(a, self.a_out, mem_in)
+        self.alu_out = self.alu.compute(c1, c2, c3, c4, c5, c6, self.d_out, a_or_m_mux_out)
 
-a_or_m_mux_out = a_or_m_mux.compute(a, a_in, mem_in)
-alu_out = alu.compute(c1, c2, c3, c4, c5, c6, a_out, d_out)
+        self.jump_decoder_out = self.jump_decoder.compute(self.alu_out, j1,j2,j3)
+        self.load_pc = self.load_jump_mux.compute(x, 0, self.jump_decoder_out)
+        instr_dir = self.pc.compute(self.a_out, 1, self.load_pc, reset)
+        d_load = self.load_d_mux.compute(x, 0, d2)
+        self.d_out = self.d_register.compute(self.alu_out, d_load)
 
-jump_decoder_out = jump_decoder.compute(alu_out, j1,j2,j3)
+        mem_dir = self.a_out
 
-d_load = load_d_mux.compute(x, zero, d2)
-d_out = d_register.compute(alu_out, d_load)
-
+        return self.alu_out, instr_dir, mem_dir
